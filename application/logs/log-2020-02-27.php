55f403d84f8e65adf31278038ffd2978fe45d018
WebMaster-php <maanjutt425@gmail.com>
2020-02-28 18:14:15 +0500

    initial commit

---
+++ application/logs/log-2020-02-27.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-02-27 00:36:22 --> Could not find the language line "options"
+ERROR - 2020-02-27 00:36:27 --> Could not find the language line "options"
+ERROR - 2020-02-27 00:36:28 --> Could not find the language line "Services"
+ERROR - 2020-02-27 00:36:28 --> Could not find the language line "Vmaps"
+ERROR - 2020-02-27 00:36:28 --> Could not find the language line "Inventory"
+ERROR - 2020-02-27 00:36:28 --> Could not find the language line "Sales Opportunity"
+ERROR - 2020-02-27 00:36:28 --> Could not find the language line "Portfolio"
+ERROR - 2020-02-27 00:36:29 --> Could not find the language line "options"
+ERROR - 2020-02-27 00:36:29 --> Could not find the language line "Assigned to"
+ERROR - 2020-02-27 00:36:29 --> Could not find the language line "options"
+ERROR - 2020-02-27 00:36:29 --> Could not find the language line "options"
+ERROR - 2020-02-27 00:36:29 --> Could not find the language line "options"
